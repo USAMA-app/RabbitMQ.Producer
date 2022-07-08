d1b7e5f85f6da0107a4adf6bd0ef12860808f4fa
naveedekram <naveed.ekram@thesoftcube.com>
2022-07-09 04:24:55 +0500

    initial commit

---
+++ RabbitMQ.Producer/DirectExchangePublisher.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Producer
+{
+    public static class DirectExchangePublisher
+    {
+        public static void Publish(IModel channel)
+        {
+            channel.ExchangeDeclare("demo-direct-exchange",
+                   ExchangeType.Direct);
+            var count = 0;
+
+            while (true)
+            {
+                var message = new { Name = "Producer", Message = $"Hello! Count: {count}" };
+                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+                channel.BasicPublish("demo-direct-exchange", "account.init", null, body);
+                count++;
+                Thread.Sleep(1000);
+            }
+
+        }
+
+    }
+}
